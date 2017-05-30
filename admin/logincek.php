24db778475ebdccb745cdc96e467d76dbcc499a3
satekambing <jabbarbie@gmail.com>
2017-05-31 01:01:20 +0700

    sukses login.. email aja sih

--- admin/logincek.php
+++ admin/logincek.php
@@ -5,21 +5,26 @@ require_once("../config/koneksi.php");
 require_once("../config/aman.php");
 
 error_reporting(E_ALL);
-if(isset($_GET['username']) && isset($_GET['pass'])){
-  $user = $_GET['username'];
-  $pass = $_GET['pass'];
+if(isset($_POST['username']) && isset($_POST['pass'])){
+  $user = $_POST['username'];
+  $pass = $_POST['pass'];
 
   if($user == "" OR $pass == ""){
     echo (die("Data Tidak Lengkap"));
   }else{
-    $user = 4;
-    $login = $koneksi->prepare("SELECT username FROM tbl_user WHERE id_user=?");
-    $login->bind_param("i",$user);
+    $login = $koneksi->prepare("SELECT email FROM tbl_user WHERE username=?");
+    $login->bind_param("s",$user);
     $login->execute();
+    $login->store_result();
 
-    $login->bind_result($username);
+
+    $login->bind_result($email);
     $login->fetch();
-    echo $username;
+    if($login->num_rows > 0){
+      echo 1;
+    }else{
+      echo "username / password salah";
+    }
   }
 }else{
   echo "Gagal";
