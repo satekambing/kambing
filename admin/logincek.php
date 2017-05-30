093a216a45136075b480c44c33493d731507299e
satekambing <jabbarbie@gmail.com>
2017-05-31 00:29:02 +0700

    kode sndiri tapi error

--- admin/logincek.php
+++ admin/logincek.php
@@ -13,27 +13,12 @@ if(isset($_GET['username']) && isset($_GET['pass'])){
     echo (die("Data Tidak Lengkap"));
   }else{
     $user = 3;
-    echo "username ->".$user.br;
-    // cek user & pass
-    // var_dump($koneksi);
-    echo "Sate".br;
-    $stmt = $koneksi->prepare("SELECT username FROM tbl_user WHERE id_user=?");
-    /* bind parameters for markers */
-    $stmt->bind_param("i", $user);
+    $login = $koneksi->prepare("SELECT * FROM tbl_user WHERE id_user=?");
+    $login->bind_param("i",$user);
+    $login->execute();
 
-    /* execute query */
-    $stmt->execute();
-
-    /* bind result variables */
-    $stmt->bind_result($username);
-
-    /* fetch value */
-    $stmt->fetch();
-
-    printf($username);
-
-    /* close statement */
-    $stmt->close();
+    $login->fetch();
+    echo "sate".$login->num_rows;
   }
 }else{
   echo "Gagal";
