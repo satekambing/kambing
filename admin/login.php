1f3556e6532fe9918483a6a7dead9e377711060e
satekambing <jabbarbie@gmail.com>
2017-05-30 18:18:14 +0700

    Clear jss / koment2nya

--- admin/login.php
+++ admin/login.php
@@ -4,18 +4,12 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Halaman Login | Admin</title>
-  <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="../resources/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="../../plugins/iCheck/square/blue.css">
+
 
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
@@ -33,7 +27,7 @@
     <form action="logincek.php" method="post">
       <div class="form-group has-feedback">
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
-        <input type="email" class="form-control" placeholder="Email / Nip">
+        <input type="email" class="form-control" autofocus="" placeholder="Email / Nip">
       </div>
       <div class="form-group has-feedback">
         <input type="password" class="form-control" placeholder="Password">
@@ -43,33 +37,15 @@
         <div class="col-xs-8">
           <a href="#">Lupa Password ?</a>
         </div>
-        <!-- /.col -->
         <div class="col-xs-4">
           <button type="submit" class="btn btn-primary btn-block btn-flat">Masuk</button>
         </div>
-        <!-- /.col -->
       </div>
     </form>
-    <!-- /.social-auth-links -->
   </div>
-  <!-- /.login-box-body -->
 </div>
-<!-- /.login-box -->
 
-<!-- jQuery 3.1.1 -->
-<script src="../../plugins/jQuery/jquery-3.1.1.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="../../bootstrap/js/bootstrap.min.js"></script>
-<!-- iCheck -->
-<script src="../../plugins/iCheck/icheck.min.js"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
-    });
-  });
-</script>
+<script src="../resources/js/jquery-3.2.1.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
