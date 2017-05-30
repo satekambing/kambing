33ebf0b5a718623a815aedcc6d51517e63e4188c
satekambing <jabbarbie@gmail.com>
2017-05-31 03:54:00 +0700

    udah fix tinggal pesan errornya

--- config/fungsi_keamanan.php
+++ config/fungsi_keamanan.php
@@ -2,26 +2,34 @@
 /**
   * Keterangan : Fungsi untuk menangani keamanana sistem
 **/
-function levelUser($angka){
-  $kelas_user   = array(
-                    1 => 'Super Admin',
-                    2 => 'Kepala BKP',
-                    3 => 'Kasubag',
-                    4 => 'Pegawai'
-                  );
-  return $kelas_user[$angka];
+function levelUser(int $angka){
+  switch ($angka) {
+    case 1:
+      return ["Super Admin",SUPER_ADMIN];
+      break;
+    case 2:
+      return ["Kepala BKP",KEPALA_BKP];
+      break;
+    case 3:
+      return ["Kasubag",KASUBAG];
+    break;
+    case 4:
+      return ["Pegawai",PEGAWAI];
+    break;
+  }
 }
 function cekLogin(){
   if(!isset($_SESSION['user']) OR $_SESSION['user'] == ''){
     echo die("Belum Login ");
   }
 }
-function cekLevel(array $angkaLevel){
-  // misal 4 = pegawai
-  cekLogin();
-  if(!in_array($_SESSION['level'], $angkaLevel)){
-    // kalau level user ada di daftar white list halaman
-    echo die(PesanPeringatan(array('jenis'=>'warning','judul'=>'Akses Dilarang','isipesan'=>'User tidak berhak mengakses halaman ini')));
+function cekLevel($halaman){
+  // echo SERVER;
+  $ceklevel  = levelUser($_SESSION['level']);
+  $level     = $ceklevel[1]; // mengambil konstanta berdasarkan user yg login saat itu juga
+
+  if(!in_array($halaman, $level)){
+    
   }
 }
 ?>
