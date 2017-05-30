2762addd1e99d561ffda69d0b4645252cbd7ba53
satekambing <jabbarbie@gmail.com>
2017-05-31 02:48:28 +0700

    cek level manual

--- config/fungsi_keamanan.php
+++ config/fungsi_keamanan.php
@@ -2,12 +2,26 @@
 /**
   * Keterangan : Fungsi untuk menangani keamanana sistem
 **/
-function hakAkses($angka){
+function levelUser($angka){
   $kelas_user   = array(
                     1 => 'Super Admin',
                     2 => 'Kepala BKP',
                     3 => 'Kasubag',
                     4 => 'Pegawai'
                   );
+  return $kelas_user[$angka];
+}
+function cekLogin(){
+  if(!isset($_SESSION['user']) OR $_SESSION['user'] == ''){
+    echo die("Belum Login ");
+  }
+}
+function cekLevel(array $angkaLevel){
+  // misal 4 = pegawai
+  cekLogin();
+  if(!in_array($_SESSION['level'], $angkaLevel)){
+    // kalau level user ada di daftar white list halaman
+    echo die(PesanPeringatan(array('jenis'=>'warning','judul'=>'Akses Dilarang','isipesan'=>'User tidak berhak mengakses halaman ini')));
+  }
 }
 ?>
