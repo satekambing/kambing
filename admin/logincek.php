29ff3f8287502ced700ed87a8746319d868e60d9
satekambing <jabbarbie@gmail.com>
2017-05-31 00:23:53 +0700

    Berhasil pke contoh

--- admin/logincek.php
+++ admin/logincek.php
@@ -2,6 +2,9 @@
 <?php
 require_once("../config/config.php");
 require_once("../config/koneksi.php");
+require_once("../config/aman.php");
+
+error_reporting(E_ALL);
 if(isset($_GET['username']) && isset($_GET['pass'])){
   $user = $_GET['username'];
   $pass = $_GET['pass'];
@@ -9,17 +12,28 @@ if(isset($_GET['username']) && isset($_GET['pass'])){
   if($user == "" OR $pass == ""){
     echo (die("Data Tidak Lengkap"));
   }else{
+    $user = 3;
+    echo "username ->".$user.br;
     // cek user & pass
-    var_dump($koneksi);
-    $loginx = $koneksi->query("SELECT username,level FROM tbl_user WHERE username=?");
-    $loginx->bind_param('ss',$user,$user);
-    echo "ada";
+    // var_dump($koneksi);
+    echo "Sate".br;
+    $stmt = $koneksi->prepare("SELECT username FROM tbl_user WHERE id_user=?");
+    /* bind parameters for markers */
+    $stmt->bind_param("i", $user);
 
-    if ($loginx->execute()){
-      echo 1;
-    }else{
-      echo "Gagal";
-    }
+    /* execute query */
+    $stmt->execute();
+
+    /* bind result variables */
+    $stmt->bind_result($username);
+
+    /* fetch value */
+    $stmt->fetch();
+
+    printf($username);
+
+    /* close statement */
+    $stmt->close();
   }
 }else{
   echo "Gagal";
