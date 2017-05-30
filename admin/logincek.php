b35e350e026e68b89280020c907a37fc8eecd7d9
satekambing <jabbarbie@gmail.com>
2017-05-31 01:28:46 +0700

    fix

--- admin/logincek.php
+++ admin/logincek.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once("../config/config.php");
 require_once("../config/koneksi.php");
 
@@ -10,16 +11,20 @@ if(isset($_POST['username']) && isset($_POST['pass'])){
   if($user == "" OR $pass == ""){
     echo (die("Data Tidak Lengkap"));
   }else{
-    $login = $koneksi->prepare("SELECT email FROM tbl_user WHERE username=?");
-    $login->bind_param("s",$user);
+    $login = $koneksi->prepare("SELECT username,pass,level FROM tbl_user WHERE (username=? OR email=?) AND pass=?");
+    $pass  = md5($pass); // konfersi ke md5 dahulu
+
+    $login->bind_param("sss",$user,$user,$pass);
     $login->execute();
 
     $login->store_result();
-    $login->bind_result($email);
-
+    $login->bind_result($username,$pass,$level);
+    $login->fetch();
     if($login->num_rows > 0){
-      // set session dulu
-      
+      // set session dulu kalo login berhasil
+      $_SESSION['user'] = $username;
+      $_SESSION['pass'] = $pass;
+      $_SESSION['level'] = $level;
       echo 1;
     }else{
       echo "username / password salah";
