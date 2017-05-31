5760348a91b51e8bdad63e79e6cae6e660512dd8
satekambing <jabbarbie@gmail.com>
2017-06-01 02:08:24 +0700

    fix ada validasi email,user

--- admin/page/user/simpanuser.php
+++ admin/page/user/simpanuser.php
@@ -1,37 +1,65 @@
 <?php
-
-if (empty($_POST['username']) OR empty($_POST['pass']) OR empty($_POST['level'])){
+if ((!isset($_POST['hapus'])) && (empty($_POST['username']) OR empty($_POST['namalengkap']) OR empty($_POST['email']) OR empty($_POST['pass']) )){
   echo die("Data tidak lengkap");
 }
+
 require_once("../../../config/fungsi_admindata.php");
 require_once("../../../config/config.php");
-
+require_once("../../../config/koneksi.php");
+require_once("../../../config/fungsi_adminview.php");
+require_once("../../../config/fungsi_keamanan.php");
+session_start();
+// cekLevel('user'); // cek status user.. sudah login / belum.. di izinkan mengakses / tidak
 extract($_POST);
 $namatable = 'tbl_user';
+$pk        =  'id_user';
 
-if ($level == 4){
-    $level = CariPegawai($username); // konvert nilai id_pegawai ke nip
+// $tanggal_user = UbahTanggal($tanggal_user??'');
+if(isset($tambah)){
+  // Jika proses tambah data
 
-    if($level == 0){
-      echo die('Nip tidak ditemukan');
-    }
+  // Cek Format Email
+  $cekEmail = cekEmail($email);
+  if ($cekEmail == "salah") {echo die("Format Email Salah");}
+
+  // Cek Duplikasi data
+  $cekUser = cekDuplikasiData($namatable,'username',$username);
+  if ($cekUser > 0) {echo die("Data Username <b>".$username.'</b> Sudah ada');}
+
+  $cekUser = cekDuplikasiData($namatable,'Email',$email);
+  if ($cekUser > 0) {echo die("Data Email <b>".$email.'</b> Sudah ada');}
+
+  $level   = 4;
+  $status  = 1;
+  $pass    = md5($pass);
+  // echo die($username.'-'.$namalengkap.'-'.$email.'-'.$pass.'-'.$level.'-'.$status);
+  $sql   = "INSERT INTO $namatable (username,namalengkap,email,pass,level,status) ";
+  $sql  .= " VALUES(?, ?, ?, ?, ?, ?)";
+  $query = $koneksi->prepare($sql);
+  $query->bind_param('ssssii',$username,$namalengkap,$email,$pass,$level,$status);
+  $page = 1;
 }
-
-require("../../../config/koneksi.php");
-// kasih proteksi
-// echo die(var_dump($koneksi));
-$sql   = "INSERT INTO $namatable (username, namalengkap, email, pass, level, status) ";
-$sql  .= " VALUES(?, ?, ?, ?, ?, ?)";
-$query = $koneksi->prepare($sql);
-
-$tanggal_pensiun    = UbahTanggal($tanggal_pensiun);
+elseif (isset($ubah)){
+  // Jika proses edit data
+  $sql   = "UPDATE $namatable SET tanggal_user=?,=?,=? WHERE $pk=?";
+  $query = $koneksi->prepare($sql);
+  $query->bind_param('sssi',$tanggal_user);
+  $page = 2;
+}
+elseif (isset($hapus)){
+  // Jika proses hapus data
+  $sql   = "DELETE FROM $namatable WHERE $pk=?";
+  $query = $koneksi->prepare($sql);
+  $query->bind_param('i', $hapus);
+  $page = 3;
+}
 // i = integer, s = string... bind_param('tipe data kolom..', 'isi kolom 1','isi kolom 2')
-$query->bind_param('ssssii', $username, $namalengkap, $email, $pass, $level, $status);
 // s 8 kali karna semua kolom berjumlah 8 dan semuanya bertipe string
 if($query->execute() == TRUE){ // Jika Koneksi Berhasil
-  echo 1; // 1 jika sukses
+  // Jika proses query berhasil
+  echo $page; //
 }else {
-  // echo 'Gagal -> '.$query->error; // 0 jika gagal
+  // Jika proses query gagal
   echo $query->error;
 }
 ?>
