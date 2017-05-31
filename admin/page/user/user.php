1e2a6819d3c09f828282b9d87ee04ca7ea083eb7
satekambing <jabbarbie@gmail.com>
2017-05-31 18:57:07 +0700

    user aman - belum ada combonya

--- admin/page/user/user.php
+++ admin/page/user/user.php
@@ -1,57 +1,55 @@
 <?php
-echo JudulHalaman(['Data User','Menampilkan Daftar User Pegawai ']);
-$JudulKolom = array('Username','Nama Lengkap','Email','Level','Status','Pilihan');
+cekLevel('user');
+echo JudulHalaman(['User','Menampilkan Data User']); // Judul Halaman - Deskripsi
+$JudulKolom = array('Username','Nama Lengkap','Email','Level','Pilihan'); // semua kolom di tbl
 
 // query kan table
-$sql   = " SELECT * FROM tbl_user";
-$User = $koneksi->query($sql);
-// die(var_dump($pegawai));
+$sql   = "SELECT * FROM tbl_user ORDER BY level";
+$user = $koneksi->query($sql);
+
 ?>
 <div class="content-header">
   <div class="box box-primary">
     <div class="box-header with-border">
       <!-- Tombol Tambah Data -->
-      <a href="#TambahDialog" data-toggle="modal" data-halaman="User">
-        <button class="btn btn-l btn-primary"><span><i class="fa fa-plus"></i></span> Tambah User </button>
+      <a href="#TambahModal" data-toggle="modal" data-halaman="user">
+        <button class="btn btn-l btn-primary"><span><i class="fa fa-plus"></i></span> Tambah User</button>
       </a>
-      <div class="box-tools">
-        <div class="" id="aku">
-
-        </div>
+      <div class="pull-right col-xs-6">
+        <?php //PencarianLanjutan(['tanggal']) ?>
       </div>
+
     </div>
     <div class="box-body">
       <!-- table-condensed -->
-      <table class="table table-striped ">
+      <table class="table table-striped " id="contoh">
         <thead>
           <tr>
             <?php KolomTable($JudulKolom) ?>
           </tr>
         </thead>
         <tbody>
-          <?php while ($r=$User->fetch_object()) { ?>
+          <?php while ($r=$user->fetch_object()) { ?>
             <tr >
               <td><?php echo $r->username ?></td>
               <td><?php echo $r->namalengkap ?></td>
               <td><?php echo $r->email ?></td>
-              <td><?php echo $r->level ?></td>
-              <td><?php echo UbahStatus($r->status) ?></td>
-              <td><a href="#UbahDialog" class="TombolEdit" data-toggle="modal" data-primarykey="id_pegawai" data-halaman="User" data-idnya=<?php  echo $r->id_pegawai ?> >
-                  <button class="btn btn-sm btn-success" ><i class="fa fa-edit"></i></button></a>
-                  <button class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></button>
+              <td><label class="label label-<?php echo levelUser($r->level)[2] ?>"><?php echo levelUser($r->level)[0] ?></label></td>
+              <td><a href="#TambahModal" class="TombolEdit" data-toggle="modal" data-primarykey="id_user" data-halaman="user" data-idnya="<?php  echo $r->id_user ?>" >
+                   <button class="btn btn-sm btn-success" ><i class="fa fa-edit"></i></button></a>
+                  <a href="#HapusModal" class="TombolHapus" data-toggle="modal" data-halaman="user" data-idnya="<?php  echo $r->id_user ?>">
+                   <button class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></button></a>
               </td>
             </tr>
           <?php } ?>
         </tbody>
       </table>
-    </div>
-    <div class="box-header">
 
     </div>
   </div>
 </div>
-<!-- KUMPULAN MODAL DIALOGNYA -->
-<?php //AwalModalDialog('Ubah Data Pegawai','UbahDialog') ?>
-<?php AwalModalDialog('Tambah User','TambahDialog') ?>
+<?php AwalModalDialog('TambahModal') ?>
+<?php AwalModalDialog('HapusModal') ?>
+
 
 <!-- Akhir -->
