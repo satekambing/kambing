3416251b3196f56c9e60e0c3798197108fb6dc38
satekambing <jabbarbie@gmail.com>
2017-06-01 02:10:32 +0700

    tambahan status warna tiap constan, fungsi cekmenu

--- config/fungsi_keamanan.php
+++ config/fungsi_keamanan.php
@@ -5,16 +5,16 @@
 function levelUser(int $angka){
   switch ($angka) {
     case 1:
-      return ["Super Admin",SUPER_ADMIN];
+      return ["Super Admin",SUPER_ADMIN,"danger"];
       break;
     case 2:
-      return ["Kepala BKP",KEPALA_BKP];
+      return ["Kepala BKP",KEPALA_BKP,"warning"];
       break;
     case 3:
-      return ["Kasubag",KASUBAG];
+      return ["Kasubag",KASUBAG,"info"];
     break;
     case 4:
-      return ["Pegawai",PEGAWAI];
+      return ["Pegawai",PEGAWAI,"primary"];
     break;
   }
 }
@@ -25,12 +25,18 @@ function cekLogin(){
 }
 function cekLevel($halaman){
   cekLogin();
-  
+
   $ceklevel  = levelUser($_SESSION['level']);
   $level     = $ceklevel[1]; // mengambil konstanta berdasarkan user yg login saat itu juga
 
   if(!in_array($halaman, $level)){
     echo die(PesanPeringatan(array('jenis'=>'warning','judul'=>'Akses Dilarang','isipesan'=>'User tidak berhak mengakses halaman ini')));
   }
+}
+function cekMenu($halaman){
+    $level = $_SESSION['level'];
+    if(in_array($level, $halaman)){
+      return true;
+    }
 }
 ?>
