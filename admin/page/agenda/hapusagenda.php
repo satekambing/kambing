949ace65dfba4b906473d1aee98458835c8b9431
satekambing <jabbarbie@gmail.com>
2017-05-31 16:27:17 +0700

    Fix keamananya bro

--- admin/page/agenda/hapusagenda.php
+++ admin/page/agenda/hapusagenda.php
@@ -1,17 +1,18 @@
 <?php
-  $halaman   = $_POST['jhal'] ?? '';
-  $id   = $_POST['jrow'] ?? '';
+$halaman   = $_POST['jhal'] ?? ''; // variable jhal + jrow berasal dari ajax
+$id        = $_POST['jrow'] ?? '';
 
-  require_once("../../../config/config.php");
-  require_once("../../../config/koneksi.php");
-  require_once("../../../config/fungsi_adminview.php");
+require_once("../../../config/config.php");
+require_once("../../../config/koneksi.php");
+require_once("../../../config/fungsi_adminview.php");
+require_once("../../../config/fungsi_keamanan.php");
+cekLevel('agenda'); // cek status user.. sudah login / belum.. di izinkan mengakses / tidak
 
+$namatable  = 'tbl_agenda';
+$pk         =  'id_agenda';
 
-  $namatable  = 'tbl_agenda';
-  $pk         =  'id_agenda';
-
-  $query    = $koneksi->query("SELECT * FROM $namatable WHERE $pk=$id");
-  $data     = $query->fetch_object();
+$query    = $koneksi->query("SELECT * FROM $namatable WHERE $pk=$id");
+$data     = $query->fetch_object();
 
 ?>
 
@@ -20,32 +21,24 @@
 </div>
 
 <div class="modal-body">
-<div class="pesan-form" >
-</div>
-<form class="form-horizontal" action="index.html" method="post">
-
-  <div class="form-group">
-    <label class="col-sm-3 control-label" for="">Nama Agenda</label>
-    <div class="col-sm-9">
-      <input type="text" class="form-control" disabled value="<?php echo $data->judul ?>">
-    </div>
+  <div class="pesan-form">
+    <!-- Pesan Kesalahan -->
   </div>
-
-  <div class="form-group">
-    <label class="col-sm-3 control-label" for="">Tanggal</label>
-    <div class="col-sm-9">
-      <input type="text" class="form-control" disabled value="<?php echo UbahTanggalKeBulan($data->tanggal_agenda) ?>">
+  <form class="form-horizontal" action="index.html" method="post">
+    <div class="form-group">
+      <label class="col-sm-3 control-label" for="">Nama Agenda</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" disabled value="<?php echo $data->judul ?>">
+      </div>
     </div>
-  </div>
 
-  <!-- <div class="form-group">
-    <label class="col-sm-3 control-label" for="">Status</label>
-    <div class="col-sm-9">
-      <button type="button" class="btn btn-warning btn-block" name="button">Aman - Tidak ada relasi dengan table lain</button>
+    <div class="form-group">
+      <label class="col-sm-3 control-label" for="">Tanggal</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" disabled value="<?php echo UbahTanggalKeBulan($data->tanggal_agenda) ?>">
+      </div>
     </div>
-  </div> -->
-
-</form>
+  </form>
 
 </div>
 <div class="modal-footer">
