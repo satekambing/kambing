2fc480b28c62290edda11cb9d8958bc3b4fddb22
satekambing <jabbarbie@gmail.com>
2017-05-31 16:44:16 +0700

    Fix untuk hapusmaster

---
+++ admin/page/master/hapusmaster.php
@@ -0,0 +1,48 @@
+<?php
+$halaman   = $_POST['jhal'] ?? ''; // variable jhal + jrow berasal dari ajax
+$id        = $_POST['jrow'] ?? '';
+
+require_once("../../../config/config.php");
+require_once("../../../config/koneksi.php");
+require_once("../../../config/fungsi_adminview.php");
+require_once("../../../config/fungsi_keamanan.php");
+cekLevel('master'); // cek status user.. sudah login / belum.. di izinkan mengakses / tidak
+
+$namatable  = 'tbl_master';
+$pk         =  'id_master'; // primarykey table
+
+$query    = $koneksi->query("SELECT * FROM $namatable WHERE $pk=$id");
+$data     = $query->fetch_object();
+
+?>
+
+<div class="modal-header">
+  <h4 class="modal-title">Ingin menghapus data <b> <?php echo $halaman ?></b> ? </h4>
+</div>
+
+<div class="modal-body">
+  <div class="pesan-form">
+    <!-- Pesan Kesalahan -->
+  </div>
+  <form class="form-horizontal" action="index.html" method="post">
+
+    <div class="form-group">
+      <label class="col-sm-3 control-label" for="">Nama master</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" disabled value="<?php echo $data->judul ?>">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-sm-3 control-label" for="">Tanggal</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" disabled value="<?php echo UbahTanggalKeBulan($data->tanggal_master) ?>">
+      </div>
+    </div>
+  </form>
+
+</div>
+<div class="modal-footer">
+    <button type='button' class='btn btn-simpan btn-danger btn-hapus'>Hapus</button>
+    <button type='button' class='btn btn-faded pull-left' data-dismiss='modal'>Kembali</button>
+</div>
