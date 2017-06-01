fbe807fdb7e73a46a25b82dde576ca1a7727b741
satekambing <jabbarbie@gmail.com>
2017-06-01 23:32:20 +0700

    bug aja

--- debug.php
+++ debug.php
@@ -1,11 +1,12 @@
 <?php
+include("config/config.php");
 include("config/fungsi_admindata.php");
-if (isset($_FILES['foto'])){
-  echo $_FILES['foto']['name'];
-  uploadfoto($_FILES['foto'],'berita');
+if (isset($_FILES['gambar'])){
+  echo $_FILES['gambar']['name'];
+  uploadfoto($_FILES['gambar'],'berita');
 }
 ?>
 <form class="" action="debug.php" method="post" enctype="multipart/form-data">
-  <input type="file" name="foto">
+  <input type="file" name="gambar">
   <input type="submit" name="simpan" value="simpan">
 </form>
