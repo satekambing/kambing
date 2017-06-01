2a0463295852364ddcca687c224856d098ba5728
satekambing <jabbarbie@gmail.com>
2017-06-02 00:41:25 +0700

    fix cuti

--- admin/page/cuti/cuti.php
+++ admin/page/cuti/cuti.php
@@ -1,58 +1,59 @@
 <?php
-echo JudulHalaman(['Data Cuti Pegawai','Menampilkan Daftar Cuti Pegawai ']);
-$JudulKolom = array('NIP','Nama Pegawai','Tanggal Mulai - Akhir','Jenis Cuti','Keterangan');
+cekLevel('cuti');
+echo JudulHalaman(['Cuti','Menampilkan data Cuti Pegawai']); // Judul Halaman - Deskripsi
+$JudulKolom = array('NIP','Nama','Jenis','No Surat','Tgl Surat','Tgl Mulai - Selesai','Keterangan','Pilihan'); // semua kolom di tbl
 
-// query kan table
-$sql   = " SELECT * FROM tbl_pegawai p INNER JOIN tbl_cuti c ON p.id_pegawai = c.id_pegawai ";
-$pegawai = $koneksi->query($sql);
-// die(var_dump($pegawai));
+// query kan table - left -> semua yg tampil di kiri akan di tampilkan
+$sql   = " SELECT * FROM tbl_cuti c LEFT JOIN tbl_pegawai p ON c.id_pegawai = p.id_pegawai";
+$cuti = $koneksi->query($sql);
+
 ?>
 <div class="content-header">
   <div class="box box-primary">
     <div class="box-header with-border">
       <!-- Tombol Tambah Data -->
-      <a href="#TambahDialog" data-toggle="modal" data-halaman="cuti">
-        <button class="btn btn-l btn-primary"><span><i class="fa fa-plus"></i></span> Tambah Data Cuti Pegawai</button>
+      <a href="#TambahModal" data-toggle="modal" data-halaman="cuti">
+        <button class="btn btn-l btn-primary"><span><i class="fa fa-plus"></i></span> Tambah Cuti</button>
       </a>
-      <div class="box-tools">
-        <div class="" id="aku">
-          <input type="text" name="" id="a" value=""><button id="zzz" type="btn btn-info" name="button">anu</button>
-        </div>
+      <div class="pull-right col-xs-6">
+        <?php PencarianLanjutan(['tanggal']) ?>
       </div>
+
     </div>
     <div class="box-body">
       <!-- table-condensed -->
-      <table class="table table-striped ">
+      <table class="table table-striped " id="contoh">
         <thead>
           <tr>
             <?php KolomTable($JudulKolom) ?>
           </tr>
         </thead>
         <tbody>
-          <?php while ($r=$pegawai->fetch_object()) { ?>
+          <?php while ($r=$cuti->fetch_object()) { ?>
             <tr >
               <td><?php echo $r->nip ?></td>
               <td><?php echo $r->nama ?></td>
-              <td><?php echo $r->tanggal_mulai.' s/d '.$r->tanggal_selesai ?></td>
               <td><?php echo $r->jenis_cuti ?></td>
+              <td><?php echo $r->no_surat ?></td>
+              <td><?php echo UbahTanggalKeBulan($r->tanggal_surat) ?></td>
+              <td><?php echo UbahTanggalKeBulan($r->tanggal_mulai).' s/d '.UbahTanggalKeBulan($r->tanggal_selesai) ?></td>
               <td><?php echo $r->keterangan ?></td>
-              <td><a href="#UbahDialog" class="TombolEdit" data-toggle="modal" data-primarykey="id_cuti" data-halaman="cuti" data-idnya=<?php  echo $r->id_pegawai ?> >
-                  <button class="btn btn-sm btn-success" ><i class="fa fa-edit"></i></button></a>
-                  <button class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></button>
+
+              <td><a href="#TambahModal" class="TombolEdit" data-toggle="modal" data-primarykey="id_cuti" data-halaman="cuti" data-idnya="<?php  echo $r->id_cuti ?>" >
+                   <button class="btn btn-sm btn-success" ><i class="fa fa-edit"></i></button></a>
+                  <a href="#HapusModal" class="TombolHapus" data-toggle="modal" data-halaman="cuti" data-idnya="<?php  echo $r->id_cuti ?>">
+                   <button class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></button></a>
               </td>
             </tr>
           <?php } ?>
         </tbody>
       </table>
-    </div>
-    <div class="box-header">
 
     </div>
   </div>
 </div>
-<!-- KUMPULAN MODAL DIALOGNYA -->
-<?php //AwalModalDialog('Ubah Data Pegawai','UbahDialog') ?>
-<?php AwalModalDialog('Ubah Data Cuti','UbahDialog') ?>
-<?php AwalModalDialog('Tambah Data Cuti','TambahDialog') ?>
+<?php AwalModalDialog('TambahModal') ?>
+<?php AwalModalDialog('HapusModal') ?>
+
 
 <!-- Akhir -->
