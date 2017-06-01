91af7c5eb45cec807bc0c4b92c310a120bffc11d
satekambing <jabbarbie@gmail.com>
2017-06-02 01:38:47 +0700

    awal tpi error

---
+++ admin/page/searchpegawai.php
@@ -0,0 +1,16 @@
+<?php
+require_once("../../../config/config.php");
+require_once("../../../config/koneksi.php");
+$q = $_GET['q'];
+
+$query = $koneksi->query("select nip as id, CONCAT_WS (' - ', nip, nama) as text from tbl_pegawai where nip like '%".$q."%'");
+$num = $query->num_rows;
+if($num > 0){
+	while($data = $query->fetch_assoc()){
+    $tmp[]    = $data;
+	}
+} else $tmp = array();
+
+echo json_encode($tmp);
+
+?>
