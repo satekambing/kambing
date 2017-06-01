633433a1b30ac4b04bc3a478078156f0dc49a341
satekambing <jabbarbie@gmail.com>
2017-06-02 02:17:41 +0700

    debug aja

--- debug.php
+++ debug.php
@@ -1,12 +1,5 @@
 <?php
 include("config/config.php");
-include("config/fungsi_admindata.php");
-if (isset($_FILES['gambar'])){
-  echo $_FILES['gambar']['name'];
-  uploadfoto($_FILES['gambar'],'berita');
-}
+include("config/fungsi_fungsibasic.php");
+
 ?>
-<form class="" action="debug.php" method="post" enctype="multipart/form-data">
-  <input type="file" name="gambar">
-  <input type="submit" name="simpan" value="simpan">
-</form>
