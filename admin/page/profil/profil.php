3892d9bb73d988c64e9a0d709a222468398c9d4d
satekambing <jabbarbie@gmail.com>
2017-06-03 01:30:18 +0700

    fix dah profil

--- admin/page/profil/profil.php
+++ admin/page/profil/profil.php
@@ -5,6 +5,9 @@ $profil = $koneksi->query($sql);
 $r      = $profil->fetch_object();
 ?>
 <div class="content-header">
+  <div class="pesan-form">
+
+  </div>
   <div class="nav-tabs-custom">
       <ul class="nav nav-tabs">
         <li class="active"><a href="#tab_1" data-toggle="tab" aria-expanded="true">Visi Misi</a></li>
@@ -14,15 +17,33 @@ $r      = $profil->fetch_object();
       </ul>
       <div class="tab-content">
         <div class="tab-pane active" id="tab_1">
-          <?php  ?>
+          <form method="POST" class="form formEditor" id="formvisi_misi">
+            <textarea name="visi_misi" class="EditorMCE" id="textvisi_misi" rows="15" cols="80">
+              <?php echo ($r->visi_misi??'')?>
+            </textarea>
+            <br />
+          </form>
+            <button name="submitbtn" class="btn btn-primary btnEditor" data-halaman="profil" data-kolom="visi_misi" >Simpan</button>
         </div>
         <!-- /.tab-pane -->
         <div class="tab-pane" id="tab_2">
-          bagian tugas pokok
+            <form method="POST" class="form formEditor" id="formtugas_pokok">
+              <textarea name="tugas_pokok" class="EditorMCE" id="texttugas_pokok" rows="100" cols="80">
+                <?php echo ($r->tugas_pokok??'')?>
+              </textarea>
+              <br />
+            </form>
+              <button name="submitbtn" class="btn btn-primary btnEditor" data-halaman="profil" data-kolom="tugas_pokok" >Simpan</button>
         </div>
         <!-- /.tab-pane -->
         <div class="tab-pane" id="tab_3">
-          bagian fungsi
+            <form method="POST" class="form formEditor" id="formfungsi">
+              <textarea name="fungsi" class="EditorMCE" id="textfungsi" rows="22" cols="80">
+                <?php echo ($r->fungsi??'')?>
+              </textarea>
+              <br />
+            </form>
+              <button name="submitbtn" class="btn btn-primary btnEditor" data-halaman="profil" data-kolom="fungsi" >Simpan</button>
         </div>
         <!-- /.tab-pane -->
       </div>
