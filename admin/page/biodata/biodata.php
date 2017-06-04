a0b3ed410d0fb8c936f75abecb34f793700b9702
satekambing <jabbarbie@gmail.com>
2017-06-05 05:31:02 +0700

    biodata pke js load

--- admin/page/biodata/biodata.php
+++ admin/page/biodata/biodata.php
@@ -1,16 +1,13 @@
 <?php
 cekLevel('biodata');
 echo JudulHalaman(['BIODATA','Menampilkan Biodata Pegawai']); // Judul Halaman - Deskripsi
-$sql    = " SELECT * FROM tbl_pegawai WHERE nip='".$_SESSION['user']."'";
-$profil = $koneksi->query($sql);
-$r      = $profil->fetch_object();
+
 // $profil->close();
 require("../config/fungsi_basic.php");
 
 ?>
 <div class="content-header">
   <div class="pesan-form">
-
   </div>
   <div class="row">
     <div class="col-md-3">
@@ -37,7 +34,6 @@ require("../config/fungsi_basic.php");
         <!-- /.box-body -->
       </div>
 
-      <!-- zz -->
       <div class="box box-info">
             <div class="box-header with-border">
               <h3 class="box-title">Aktifitas Terakhir</h3>
@@ -83,84 +79,14 @@ require("../config/fungsi_basic.php");
       <div class="nav-tabs-custom">
 
           <ul class="nav nav-tabs">
-            <li class="active"><a href="#tab_1" data-toggle="tab" aria-expanded="true">Biodata</a></li>
-            <li class=""><a href="#tab_2" data-toggle="tab" aria-expanded="false">Data Keluarga</a></li>
+            <li class="active"><a href="#tab_biodata" data-toggle="tab" aria-expanded="true" data-halaman="biodata" data-form="biodata_detail">Biodata</a></li>
+            <li class=""><a href="#tab_2" data-toggle="tab" aria-expanded="false" data-halaman="biodata">Data Keluarga</a></li>
             <li class=""><a href="#tab_3" data-toggle="tab" aria-expanded="false">Riwayat Pendidikan</a></li>
             <!-- <li class="pull-right"><a href="#" class="text-muted"><i class="fa fa-gear"></i></a></li> -->
           </ul>
           <div class="tab-content">
-            <div class="tab-pane active" id="tab_1" >
-              <form class="form" action="" method="post">
+            <div class="tab-pane active tab_awal" id="tab_biodata" >
 
-                <div class="form-group">
-                  <label for="">NIP</label>
-                  <input type="text" name="nip"  class="form-control" placeholder="Nomor Induk Pegawai" autofocus value="<?php echo ($r->nip ?? '') ?>"  >
-                </div>
-                <div class="form-group">
-                  <label for="">Nama Lengkap</label>
-                  <input type="text" name="nama"  class="form-control" placeholder="Nama Lengkap" value="<?php echo ($r->nama ?? '') ?>"  >
-                </div>
-                <div class="form-group">
-                  <label for="">Jenis Kelamin</label>
-                  <div class="radio">
-
-                    <label>
-                      <input type="radio" name="jenis_kelamin" id="jenis_kelaminl" value="L" checked="">
-                      Laki - Laki
-                    </label> &nbsp
-                    <label class="radio-inline">
-                      <input type="radio" name="jenis_kelamin" id="jenis_kelaminp" value="P" >
-                      Perempuan
-                    </label>
-                  </div>
-
-                </div>
-                <div class="form-group ">
-
-                  <div class="row">
-                    <div class="col-sm-5">
-                      <label for="">Tempat </label>
-                      <input type="text" name="tempat_lahir"  class="form-control" placeholder="Tempat Lahir" value="<?php echo ($r->tempat_lahir ?? '') ?>"  >
-                    </div>
-                    <div class="col-sm-7">
-                      <label for="">Tanggal Lahir</label>
-
-                    <div class="input-group date">
-                      <input type="text" name="tanggal_lahir" placeholder="YYYY-DD-MM" value="<?php echo UbahTanggalKeView($r->tanggal_lahir??'') ?>"  class="tanggalx form-control" >
-                      <span class="input-group-addon text-blue"><i class="fa fa-calendar"></i></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-                <div class="form-group">
-                  <label for="">Agama</label>
-                  <select class="form-control" name="agama">
-                    <?php KolomAgama($r->agama ?? '') ?>
-                  </select>
-                </div>
-                <div class="form-group">
-                  <label for="">No Telp</label>
-                  <input type="text" name="no_telp"  class="form-control" placeholder="No Telp" value="<?php echo ($r->no_telp ?? '') ?>"  >
-                </div>
-                <div class="form-group">
-                  <label for="">Email</label>
-                  <input type="email" name="email"  class="form-control" placeholder="Contoh test@gmail.com" value="<?php echo ($r->email ?? '') ?>"  >
-                </div>
-                <div class="form-group">
-                  <label for="">Foto</label>
-                  <input type="file" name="gambar" class="form-control">
-                  <?php if(isset($r->foto_profil) && (!$r->foto_profil == "")){ ?>
-                    <input type="hidden" name="gambar2" value="<?php echo $r->foto_profil ?>">
-                    <div class="pull-right">
-                      <p class="text-warning">* Kosongkan File Jika Tidak ingin mengganti</p>
-                    </div>
-                      <img class="img-thumbnail " src="../files/foto_pegawai/<?php echo $r->foto_profil ?>" width="150px" alt="">
-                  <?php } ?>
-                </div>
-
-              </form>
-                <button name="submitbtn" class="btn btn-primary btnEditor" data-halaman="profil" data-kolom="visi_misi" >Simpan</button>
             </div>
             <!-- /.tab-pane -->
             <div class="tab-pane" id="tab_2">
@@ -195,7 +121,7 @@ require("../config/fungsi_basic.php");
                   <div class="form-group">
                     <label for="" class="col-sm-3"></label>
                     <div class="col-sm-9">
-                      <input type="submit" name="buttonsave" value="Simpan">
+                      <input type="submit" class="btn btn-primary" name="buttonsave" value="Simpan Data Keluarga">
                     </div>
                   </div>
                 </form>
